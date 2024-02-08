e30ebf7f574a7f62c6b28cbb1e4ab60f878eb74e
Kingofhollows099 <kingofhollows099@gmail.com>
2024-02-07 18:44:10 -0600

    Made Autos need to finish RightTwoPieceRight.java and RightTwoPieceLeft.java and add them to Autos.java

--- src/main/java/frc/robot/auto/Autos.java
+++ src/main/java/frc/robot/auto/Autos.java
@@ -8,11 +8,10 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.auto.pathing.FollowPath;
-import frc.robot.auto.paths.ExamplePath;
-import frc.robot.auto.paths.StartToSecondNote;
 import frc.robot.auto.util.SetDrivePosition;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.auto.paths.*;
 
 public enum Autos {
 
@@ -36,7 +35,7 @@ public enum Autos {
             new FollowPath(new ExamplePath())
         )
     ),
-     TwoNoteAuto(
+     StartToSecondNote(
         new SequentialCommandGroup(
 
             /**
@@ -45,12 +44,93 @@ public enum Autos {
              * initially to (0, 0)
              */
             new SetDrivePosition(new Pose2d(1.0,6.0,new Rotation2d(0))),
+                //angle to AprilTag and shoot
                 new ExampleCommand(new ExampleSubsystem()), //Shooter.getInstance()
 
             // This command will run until the end of the path is reached.
             new FollowPath(new StartToSecondNote()) 
         )
-    );
+    ),
+    ShootLeft(
+        new SequentialCommandGroup(
+
+            /**
+             * This command sets the original position of the robot,
+             * as when the robot powers on it will set its position
+             * initially to (0, 0)
+             */
+            new SetDrivePosition(new Pose2d(1.946,7.054,new Rotation2d(0))), //starts at these points (might need to tweak)
+                //angle to AprilTag and shoot
+                new ExampleCommand(new ExampleSubsystem()), //Shooter.getInstance()
+
+            // This command will run until the end of the path is reached.
+            new FollowPath(new ShootLeft()) 
+        )
+    ),
+    ShootCenter(
+        new SequentialCommandGroup(
+
+            /**
+             * This command sets the original position of the robot,
+             * as when the robot powers on it will set its position
+             * initially to (0, 0)
+             */
+            new SetDrivePosition(new Pose2d(1.946,4.176,new Rotation2d(0))), //starts at these points (might need to tweak)
+                //angle to AprilTag and shoot
+                new ExampleCommand(new ExampleSubsystem()), //Shooter.getInstance()
+
+            // This command will run until the end of the path is reached.
+            new FollowPath(new ShootCenter()) 
+        )
+    ),
+    ShootRight(
+        new SequentialCommandGroup(
+
+            /**
+             * This command sets the original position of the robot,
+             * as when the robot powers on it will set its position
+             * initially to (0, 0)
+             */
+            new SetDrivePosition(new Pose2d(1.946,1.622,new Rotation2d(0))), //starts at these points (might need to tweak)
+                //angle to AprilTag and shoot
+                new ExampleCommand(new ExampleSubsystem()), //Shooter.getInstance()
+
+            // This command will run until the end of the path is reached.
+            new FollowPath(new ShootRight()) 
+        )
+    ),
+    LeftBackUpTwoPiece(
+        new SequentialCommandGroup(
+
+            /**
+             * This command sets the original position of the robot,
+             * as when the robot powers on it will set its position
+             * initially to (0, 0)
+             */
+            new SetDrivePosition(new Pose2d(1.946,7.054,new Rotation2d(0))), //starts at these points (might need to tweak)
+                //angle to AprilTag and shoot
+                new ExampleCommand(new ExampleSubsystem()), //Shooter.getInstance()
+
+            // This command will run until the end of the path is reached.
+            new FollowPath(new LeftBackUpTwoPiece()) 
+        )
+    ),
+    CenterBackUpTwoPiece(
+        new SequentialCommandGroup(
+
+            /**
+             * This command sets the original position of the robot,
+             * as when the robot powers on it will set its position
+             * initially to (0, 0)
+             */
+            new SetDrivePosition(new Pose2d(2.757,1.946,new Rotation2d(0))), //starts at these points (might need to tweak)
+                //angle to AprilTag and shoot
+                new ExampleCommand(new ExampleSubsystem()), //Shooter.getInstance()
+
+            // This command will run until the end of the path is reached.
+            new FollowPath(new CenterBackUpTwoPiece()) 
+        )
+    );  
 
     /* ----------------- */
 
