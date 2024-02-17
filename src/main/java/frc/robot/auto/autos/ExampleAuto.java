e2180f83a27c35433a920f8c1fa30c542b33a394
cwedin78 <cwedin78@gmail.com>
2024-02-17 12:32:28 -0600

    Example auto dev

--- src/main/java/frc/robot/auto/autos/ExampleAuto.java
+++ src/main/java/frc/robot/auto/autos/ExampleAuto.java
@@ -2,12 +2,17 @@ package frc.robot.auto.autos;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.auto.pathing.FollowPath;
+import frc.robot.auto.pathing.pathObjects.Path;
+import frc.robot.auto.pathing.pathObjects.PathPoint;
 import frc.robot.auto.paths.leftStart.twoPiece.LeftNearLeft;
 import frc.robot.auto.util.SetDrivePosition;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.MoveIntakeFirst;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 
 public class ExampleAuto extends SequentialCommandGroup{
     public ExampleAuto(){
@@ -18,10 +23,19 @@ public class ExampleAuto extends SequentialCommandGroup{
              * as when the robot powers on it will set its position
              * initially to (0, 0)
              */
-            new SetDrivePosition(new Pose2d(1.946,7.054,new Rotation2d(0))), //starts at these points (might need to tweak)
+            new SetDrivePosition(new Pose2d(0, 0, new Rotation2d(0))), //starts at these points (might need to tweak)
 
             // This command will run until the end of the path is reached.
-            new FollowPath(new LeftNearLeft())
+            new FollowPath(
+                new Path(
+                    new PathPoint(
+                        new Translation2d(0, 0),
+                        new Rotation2d(),
+                        1,
+                        new MoveIntakeFirst(IntakeSubsystem.getInstance(), null, 0, 0)
+                    )
+                )
+            )
         );
     }
 }
